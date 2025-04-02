a8ebaf110531dba6f6f9ecbfca0b210ce08017a0
Mo-moli <sabai930808@gmail.com>
2025-04-02 15:46:07 +0800

    recorder.jsx 刪除錄音圖片的文字

--- frontend/src/components/recoder.jsx
+++ frontend/src/components/recoder.jsx
@@ -1,12 +1,12 @@
 // src/components/Header.jsx
-function Recorder(isRecording,onClick){
+function Recorder({isRecording,onClick}){
   return (
     <>
     <button className="record-button" onClick={onClick}>
       {isRecording?'停止錄音':'開始錄音'}
       <img 
       src={isRecording? '/.stop.png':'.record.png'}
-      alt={isRecording? '停止錄音圖示':'開始錄音圖示'} 
+      
       />
 
       <p className="message">
