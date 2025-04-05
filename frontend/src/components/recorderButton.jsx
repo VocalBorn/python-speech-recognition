6d5e1822495d7c43a7aed17d97e00ea297354cb2
Mo-moli <sabai930808@gmail.com>
2025-04-05 14:54:25 +0800

    刪除多餘span

--- frontend/src/components/recorderButton.jsx
+++ frontend/src/components/recorderButton.jsx
@@ -9,8 +9,6 @@ function RecorderButton({isRecording,onClick}){
             alt={isRecording ? '停止錄音' : '開始錄音'}
             />
 
-            <span className="recorder-button-text">
-                {isRecording ? '停止錄音':'開始錄音'}</span>
             <p className="message">
                 {isRecording ? '停止錄音':'錄音開始'}
             </p>
