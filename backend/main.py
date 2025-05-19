1b8b30c98a6b3edf62f7d3d81322a846ab7d475a
Mo-moli <sabai930808@gmail.com>
2025-05-19 10:07:10 +0800

    修改路徑成5174

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS：允許 React 前端開發環境請求
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
